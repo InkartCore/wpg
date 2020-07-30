207af2f3096df6f2ff9ea4e29fdc64bfcd8c7078
shivam <shivamraj2197@gmail.com>
2020-07-30 18:51:58 +0530

    Added a line
    Signed-off-by: shivam <shivamraj2197@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World
-        </p>
+        <p>Hello World</p>
+        <h3>Welcome to Electrovert</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
